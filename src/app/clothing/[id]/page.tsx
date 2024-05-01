5da99937e65fc09078d6bcc28e940511e71e2d10
Nathan Hoang <46659716+nathanthomashoang@users.noreply.github.com>
2024-04-30 17:05:50 -0700

    made item view page static for GH pages deployment

--- src/app/clothing/[id]/page.tsx
+++ src/app/clothing/[id]/page.tsx
@@ -1,51 +1,59 @@
-'use client';
-
 import axios from 'axios';
 import * as React from 'react';
 import ItemView from '@/components/server/ItemView';
 import { Item } from '@/types/item';
-import CircularProgress from '@mui/material/CircularProgress';
+// import CircularProgress from '@mui/material/CircularProgress';
 
 export async function generateStaticParams() {
+    const items: Item[] = await fetch('https://my-json-server.typicode.com/nathanthomashoang/db-json-styler/clothing').then((res) => res.json());
+
+    return items.map((item) => ({
+        item: item.id,
+    }));
+}
+
+export async function getItemById(id: number) {
     try {
-      const response = await axios.get('https://my-json-server.typicode.com/nathanthomashoang/db-json-styler/clothing');
-      const items: Item[] = response.data;
-  
-      return items.map(item => ({
-        id: item.id,
-      }));
+        const response = await axios.get(`https://my-json-server.typicode.com/nathanthomashoang/db-json-styler/clothing/${id}`);
+        return response.data;
+
     } catch (error) {
-      console.error('Error fetching items:', error);
+        console.error(error);
     }
-  }
+}
 
-const page = ({ params }: { params: { id: number } }) => {
-    const [item, setItem] = React.useState<Item>();
-    const [isLoading, setIsLoading] = React.useState(true);
+const page = async ({ params }: { params: { id: number } }) => {
+    // const [item, setItem] = React.useState<Item>();
+    // const [isLoading, setIsLoading] = React.useState(true);
 
-    React.useEffect(() => {
-        async function getItemById(id: number) {
-            try {
-                const response = await axios.get(`https://my-json-server.typicode.com/nathanthomashoang/db-json-styler/clothing/${id}`);
-                setItem(response.data);
-                setIsLoading(false);
-            } catch (e) {
-                console.error(e);
-            }
-        }
-        getItemById(params.id);
-    }, []);
+    const item = await getItemById(params.id);
+
+    // React.useEffect(() => {
+    //     async function getItemById(id: number) {
+    //         try {
+    //             const response = await axios.get(`https://my-json-server.typicode.com/nathanthomashoang/db-json-styler/clothing/${id}`);
+    //             setItem(response.data);
+    //             setIsLoading(false);
+    //         } catch (e) {
+    //             console.error(e);
+    //         }
+    //     }
+    //     getItemById(params.id);
+    // }, []);
 
     return (
         <div>
-            {isLoading ?
-                <div className="flex flex-row justify-center items-center h-96">
-                    <CircularProgress />
-                </div> :
-                item && <ItemView item={item} />
-            }
+            <ItemView item={item} />
         </div>
+        // <div>
+        //     {isLoading ?
+        //         <div className="flex flex-row justify-center items-center h-96">
+        //             <CircularProgress />
+        //         </div> :
+        //         item && <ItemView item={item} />
+        //     }
+        // </div>
     )
 }
 
-export default page
+export default page;
